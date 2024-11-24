32bd3124f3d537410050ad652a5a5179e92a3756
haykam821 <24855774+haykam821@users.noreply.github.com>
2024-11-24 13:04:03 -0500

    Update minigame to Minecraft 1.20.1

--- src/main/java/io/github/haykam821/clutchbridge/Main.java
+++ src/main/java/io/github/haykam821/clutchbridge/Main.java
@@ -7,13 +7,17 @@ import net.minecraft.util.Identifier;
 import xyz.nucleoid.plasmid.game.GameType;
 
 public class Main implements ModInitializer {
-	public static final String MOD_ID = "clutchbridge";
+	private static final String MOD_ID = "clutchbridge";
 
-	private static final Identifier CLUTCH_BRIDGE_ID = new Identifier(MOD_ID, "clutch_bridge");
+	private static final Identifier CLUTCH_BRIDGE_ID = Main.identifier("clutch_bridge");
 	public static final GameType<ClutchBridgeConfig> CLUTCH_BRIDGE_TYPE = GameType.register(CLUTCH_BRIDGE_ID, ClutchBridgeConfig.CODEC, ClutchBridgeWaitingPhase::open);
 
 	@Override
 	public void onInitialize() {
 		return;
+	}
+
+	public static Identifier identifier(String path) {
+		return new Identifier(MOD_ID, path);
 	}
 }
